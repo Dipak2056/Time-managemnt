72b7d11bfa9e418e28b4965b804f3ba64f22afa3
Dipak 2056 <paudelsantosh508@gmail.com>
2022-02-15 10:34:10 +1100

    a bug in checking the total hour, need to fix

--- script.js
+++ script.js
@@ -5,21 +5,27 @@ const taskList = [];
 const badList = [];
 const hrPerweek = 168;
 
+
 const handleOnSubmit = (e) => {
     const frmData = new FormData(e);
     const task = frmData.get('task');
     const hr = +frmData.get('hr');
-
+    
+    const ttlBadHrs = totalBadHour();
+    const total1 = totalTaskHour();
+    if(hr < 1){return alert('woohooo! you cannot travel time.')};
+    if( ttlBadHrs+total1  > hrPerweek ){
+        return alert('cannot add')
+    }
     const obj = {
         task,
         hr
     }
-
-    taskList.push(obj);
-    display();
-    totalTaskHour();
     
-
+    taskList.push(obj);
+    totalTaskHour();
+    display();
+    
 };
 const display = () => {
     let str ='';
@@ -36,7 +42,7 @@ const display = () => {
                         <div class="btn btn-danger" title="Delete" onclick="deleteItem(${i})">
                             <i class="fa-solid fa-trash-can" title="Delete"></i>
                         </div>
-                        <div class="btn btn-warning " title="Mark" onclick="moveItem(${i})">
+                        <div class="btn btn-warning " title="Mark" onclick="markAsNotToDo(${i})">
                         <i class="fa-solid fa-circle-chevron-right"></i>
                         </div>
         </td>
@@ -46,24 +52,70 @@ const display = () => {
     });
     document.getElementById('task-list').innerHTML = str;
 };
+const displayBadList = () => {
+    let str ='';
+    badList.map((item,i)=>{
+        str += ` <tr>
+        <td>
+            <input type="checkbox">
+            ${item.task}
+        </td>
+        <td> 
+        ${item.hr}
+        </td>
+        <td class="text-end">
+        <div class="btn btn-warning " title="Mark" onclick="markAsNotToDo(${i})">
+        <i class="fa-solid fa-circle-chevron-left"></i>
+        </div>
+                        <div class="btn btn-danger" title="Delete" onclick="deleteBadItem(${i})">
+                            <i class="fa-solid fa-trash-can" title="Delete"></i>
+                        </div>
+        </td>
+    </tr>`
+    
+
+    });
+    document.getElementById('bad-list').innerHTML = str;
+};
 const deleteItem = (i) => {
-    const targetElement = taskList[i];
     taskList.splice(i,1);
     display();
     totalTaskHour();
+}
+const deleteBadItem = (i) => {
+    if(!confirm("Are you sure to delete")){
+        return ;
+    }
+    badList.splice(i,1);
+    displayBadList();
+    totalTaskHour();
+    totalBadHour();
 }
 const totalTaskHour = () =>{
     const total = taskList.reduce((subttal,item)=>{
          return subttal + item.hr;
         },0);
-        document.getElementById('Total').innerText = total;
-        if(total > hrPerweek){
-            alert('you cannot go more than '+hrPerweek);
-            deleteItem(taskList[taskList.length]);
-        }
-}
+        const ttlBadHrs = totalBadHour();
+        const ttlHrs = total + ttlBadHrs;
+        document.getElementById('Total').innerText = ttlHrs;
+        return total;
+    }
+const totalBadHour = () =>{
+    const total = badList.reduce((subttal,item)=>{
+         return subttal + item.hr;
+        },0);
+        document.getElementById('BadTotal').innerText = total;
+        return total;
+        
+    }
+    const markAsNotToDo = (i) =>{
+        const itm = taskList.splice(i,1);
+        display();
 
-
+        badList.push(itm[0]);
+        displayBadList();
+        totalBadHour();
+    }
 //loop the array and diplay under the tasks list
 // calculate total hours 
 //create another list bad list
